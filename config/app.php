9b9f470a27f583e973e26e9ad99df85d7a63502e
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2019-10-28 17:15:38 -0300

    speedup package development

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        DOIWeb\Providers\DOIWebServiceProvider::class,
 
         /*
          * Application Service Providers...
